fe06016488ac4b3dbe1216ae2bc94979d4c742b2
aidenseibel <aiden.seibel@gmail.com>
2024-02-24 13:50:58 -0600

    Initial Commit

---
+++ Carbonsaurus/CarbonsaurusApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarbonsaurusApp.swift
+//  Carbonsaurus
+//
+//  Created by Aiden Seibel on 2/24/24.
+//
+
+import SwiftUI
+
+@main
+struct CarbonsaurusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
